07f4b84881832835ab165199162561bd10e79c2f
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-09 19:54:24 -0400

    tailwind added for css

--- lesson-plan-gatsby/gatsby-config.js
+++ lesson-plan-gatsby/gatsby-config.js
@@ -27,6 +27,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-plugin-postcss',
+      options: {
+        postCssPlugins: [require('tailwindcss')('./tailwind.config.js')],
+      },
+    },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: { tailwind: true }
+    }
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
