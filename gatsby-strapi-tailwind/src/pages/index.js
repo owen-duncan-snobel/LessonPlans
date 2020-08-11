8c3ab1f5ec081e57ff0c14a0138b8a80c52c04b7
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-10 23:07:12 -0400

    update to index file

--- gatsby-strapi-tailwind/src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-function IndexPage() {
-  return (
-    <div></div>
-  );
-}
-
-export default IndexPage;
---
+++ gatsby-strapi-tailwind/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navigation from '../components/Header/navigation';
+
+const index = () => {
+  return (
+    <div>
+      <Navigation />
+    </div>
+  )
+}
+
+export default index;
