d5bfc59a20ef62b16ac099bfa858e383eff8f42e
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-16 03:18:15 -0400

    route updates to have registration from plans

--- gatsby-strapi-tailwind/gatsby-config.js
+++ gatsby-strapi-tailwind/gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         apiURL: 'http://localhost:1337',
         contentTypes: [ // List of the Content Types you want to be able to request from Gatsby.
           'user',
-          'plans'
+          'registrations'
         ],
         queryLimit: 1000,
       },
