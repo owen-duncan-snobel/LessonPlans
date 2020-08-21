fab619064d85bb4be3828e9c4d4a2cc22bb0ec98
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-20 23:30:35 -0400

    temporary layout placeholder for registration search bar

---
+++ gatsby-strapi-tailwind/src/components/registration/registration-search/registrationSearch.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const RegistrationSearchBar: React.FunctionComponent = () => {
+    return (
+        <div>
+            <p className=' text-md font-bold bg-red-500 text-gray-900 p-2'>Barcode Search</p>
+        </div>
+    )
+}
+export default RegistrationSearchBar;
