442d95a067737a0752fabbed3facc9ff7b25d617
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-20 23:30:00 -0400

    program layout standardizes content type and returns a standard layout across all programs

--- gatsby-strapi-tailwind/src/components/registration/registrationLayout.tsx
+++ gatsby-strapi-tailwind/src/components/registration/registrationLayout.tsx
@@ -1,11 +1,46 @@
 import React from 'react';
+interface ProgramRegistration {
+    id: number,
+    programName: string,
+    barcode: number,
+    programTime: string,
+    dayOfWeek: string,
+    programStartDate: string,
+    complex: string,
+    numberClasses: number,
+    program: Program,
+    created_at: string,
+    updated_at: string,
+}
 
-const RegistrationLayout: React.FunctionComponent = ({ children }) => {
-    console.log(children)
+interface Program {
+    id: number | null,
+    programName: string,
+    description: string,
+    created_at: string,
+    updated_at: string
+}
+const RegistrationLayout: React.FunctionComponent<ProgramRegistration> = ({
+    id,
+    programName,
+    barcode,
+    programTime,
+    dayOfWeek,
+    programStartDate,
+    complex,
+    numberClasses,
+    program,
+    created_at,
+    updated_at
+}) => {
     return (
-        <div className='flex justify-center mt-40'>
-            <div className='text-center mr-20'>
+        <div className='flex justify-center mt-10'>
+            <div className='mr-20'>
+                <p className='font-bold'> {programName} </p>
 
+                <p>
+                    {program.description}
+                </p>
             </div>
         </div>
     )
