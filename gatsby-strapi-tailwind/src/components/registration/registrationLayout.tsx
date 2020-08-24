2224f477315106f6d66e21b61129124479362263
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-24 00:00:20 -0400

    moment js added for easy standardization of times

--- gatsby-strapi-tailwind/src/components/registration/registrationLayout.tsx
+++ gatsby-strapi-tailwind/src/components/registration/registrationLayout.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
+import moment from 'moment';
+
 interface ProgramRegistration {
     id: number,
     programName: string,
@@ -7,12 +9,14 @@ interface ProgramRegistration {
     dayOfWeek: string,
     programStartDate: string,
     complex: string,
+    program: Program
     numberClasses: number,
-    program: Program,
     created_at: string,
-    updated_at: string,
+    updated_at: string
 }
-
+/**
+ *  * Program holds the properties and description for a program ex.) Swimmer 1, Swimmer 2
+ */
 interface Program {
     id: number | null,
     programName: string,
@@ -20,30 +24,51 @@ interface Program {
     created_at: string,
     updated_at: string
 }
-const RegistrationLayout: React.FunctionComponent<ProgramRegistration> = ({
-    id,
-    programName,
-    barcode,
-    programTime,
-    dayOfWeek,
-    programStartDate,
-    complex,
-    numberClasses,
-    program,
-    created_at,
-    updated_at
-}) => {
-    return (
-        <div className='flex justify-center mt-10'>
-            <div className='mr-20'>
-                <p className='font-bold'> {programName} </p>
 
-                <p>
-                    {program.description}
-                </p>
+
+const RegistrationProgram: React.FunctionComponent<{ data: ProgramRegistration[] }> = ({ data }) => {
+
+    const [toggleProgram, setToggleProgram] = useState(false);
+
+    return (
+        < div >
+            <button
+                className='bg-red-500 hover:bg-red-400 py-1 px-1 rounded mt-1 focus'
+                onClick={() => setToggleProgram(!toggleProgram)}  >
+                Show Courses
+            </button>
+            <div className={(toggleProgram ? 'block' : 'hidden')} >
+                <table className='table-auto w-full mt-2'>
+                    <thead>
+                        <tr className='bg-gray-200'>
+                            <th>Course</th>
+                            <th>Barcode</th>
+                            <th>Times</th>
+                            <th>Dates</th>
+                            <th>Complex</th>
+                            <th>Classes</th>
+                        </tr>
+                    </thead>
+
+                    {data.map(program => {
+                        const time = moment(program.programTime, 'h:mm:ss.sss').format('LT');
+                        const startDate = moment(program.programStartDate).format('MMMM Do YYYY');
+                        return (
+                            <tr>
+                                <td>{program.programName}</td>
+                                <td>{program.barcode}</td>
+                                <td>{time}</td>
+                                <td>{program.dayOfWeek + ' ' + startDate}</td>
+                                <td>{program.complex.split('_').join(' ')}</td>
+                                <td>{program.numberClasses}</td>
+                            </tr>
+                        )
+                    })}
+
+                </table>
+
             </div>
-        </div>
+        </div >
     )
 }
-
-export default RegistrationLayout;
+export default RegistrationProgram;
