ace304c06483b92a7f6fda093a27a1596dea620b
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-31 01:48:05 -0400

    generic single program layout added

--- gatsby-strapi-tailwind/src/components/programs/templates/singleProgramLayout.tsx
+++ gatsby-strapi-tailwind/src/components/programs/templates/singleProgramLayout.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import Navigation from '../../navigation';
+
 interface SingleProgram {
     id: number,
     programName: string,
@@ -11,9 +13,26 @@ interface SingleProgram {
         category: string
     }
 }
-const SingleProgram = ({ pageContext }: any) => {
+const SingleProgram: React.FunctionComponent = ({ pageContext }: any) => {
     const { data } = pageContext;
+    const category: string = data.category.category.split('_').join(' ');
+    return (
+        <div>
+            <Navigation />
+            <div className='flex w-screen justify-center'>
+                <div className='w-3/4 justify-center text-center bg-red-500 px-2'>
+                    <p className='text-lg font-bold'>{data.programName}</p>
+                    <p className='text-left'>{data.description}</p>
+                    <div className='flex flex-wrap justify-center space-x-2'>
+                        <img className='w-40 h-40'></img>
+                        <img className='w-40 h-40'></img>
+                        <img className='w-40 h-40'></img>
+                    </div>
+                    <p>#{category}</p>
+                </div>
+            </div>
 
-    return <div>{data.programName}</div>
+        </div >
+    )
 }
 export default SingleProgram;
