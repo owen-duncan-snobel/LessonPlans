49317312c9ea11baa25f93dadb765b48f08542fc
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-31 01:47:51 -0400

    generic homepage

--- gatsby-strapi-tailwind/src/pages/index.tsx
+++ gatsby-strapi-tailwind/src/pages/index.tsx
@@ -5,8 +5,16 @@ import Navigation from '../components/navigation';
 const Index: React.FunctionComponent = () => {
 
   return (
-    <div className='h-screen'>
+    <div className='h-screen bg-red-500'>
       <Navigation />
+      <div className='flex flex-wrap justify-center'>
+        <div className='w-screen text-7xl text-center font-extrabold pt-16'>
+          Lesson Plans
+        </div>
+        <div className='text-3xl italic text-indigo-700'>
+          demo
+        </div>
+      </div>
     </div>
   )
 }
