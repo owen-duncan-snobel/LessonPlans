e59b277c32489b7e39d387ffaca10e24ff1f1e79
owen-duncan-snobel <oadster@cse.yorku.ca>
2020-08-30 22:04:21 -0400

    update to model to dynamically create slugs for every new peice of content created

--- strapi-cms/api/program/models/program.js
+++ strapi-cms/api/program/models/program.js
@@ -1,8 +1,15 @@
 'use strict';
+const slugify = require('slugify');
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/models.html#lifecycle-hooks)
- * to customize this model
- */
-
-module.exports = {};
+module.exports = {
+    lifecycles: {
+        async beforeCreate(data) {
+            strapi.log.debug("beforeCreate")
+            data.slug = slugify(data.programName, { lower: true });
+        },
+        async beforeUpdate(params, data) {
+            strapi.log.debug("beforeUpdate")
+            data.slug = slugify(data.programName, { lower: true });
+        },
+    },
+};
